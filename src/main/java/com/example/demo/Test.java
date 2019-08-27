940203d36b15c0dddaeb826566e592c681fc6d03
wangyanfei <813.wangyanfei@163.com>
2019-08-27 17:32:34 +0800

    测试提交

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @author wangyanfei
+ * @description Test
+ * @date 2019-08-27 17:30
+ **/
+public class Test {
+     public void testSubmit(){
+         System.out.println("This is my First Submit");
+     }
+}
